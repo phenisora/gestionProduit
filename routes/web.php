b00d516df23b19bd8045dad1661773b3f58788d3
Khoudia Niang <niang62@yahoo.com>
2025-12-18 17:41:05 +0000

    ajout de la route delete au niveau de lindex

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/dashboard',[ProductController::class,'dashboard'])->name('dashboard
 Route::get('/products',[ProductController::class,'index'])->name('products.index');
 Route::get('/products/create',[ProductController::class,'create'])->name('products.create');
 Route::post('/products/store',[ProductController::class,'store'])->name('products.store');
-Route::delete('/products/{id}',[ProductController::class,'destroy'])->name('products.delete');
+Route::delete('/products/{id}',[ProductController::class,'delete'])->name('products.delete');
 Route::get('/products/edit',[ProductController::class,'edit'])->name('products.edit');
 
 //Route pour la page d'acceuil
