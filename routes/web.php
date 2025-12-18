b6ec4b5c11a0d2d7318e3ea665609b037a41245c
Moi Khady <khadykebecamara@example.com>
2025-12-18 16:04:43 +0100

    Sonouna

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductConctroller;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Pour less etudiants
+Route::get('products', [ProductConctroller::class,'index'])->name('products.index');
+Route::get('/products/create', [ProductConctroller::class,'create'])->name('products.create');
+Route::post('/products/store',[ProductConctroller::class,'store'])->name('products.store');
+
+
+
